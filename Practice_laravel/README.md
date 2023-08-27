1862f751859d0affd45a8a7a8570f7502c520972
DipItlogiko <dipitlogiko@gmail.com>
2023-08-27 11:10:52 +0600

    Facades part 1

--- Practice_laravel/README.md
+++ Practice_laravel/README.md
@@ -1,1 +1,28 @@
 # Laravel-10Documentation
+
+## Facades
+
+  * laravel a facade hocche she service container ar moddhe amader service gulo registar kore rakhe
+
+  tar por amra jokhon facade call kore method take call kori tokhon she oi method take niye ashe
+   
+  Non static method take static vabe call korte Facades help kore
+
+  ## We will see it in 3 ways:
+    
+  * row php example
+
+    go route/web.php
+
+    
+  * How laravel facade works example
+
+    go route/web.php
+
+## Need to see this video
+
+[Link](https://www.youtube.com/watch?v=bR0pW4IQmPw)
+
+
+
+
--- Practice_laravel/routes/web.php
+++ Practice_laravel/routes/web.php
@@ -13,6 +13,88 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+//// Row php example----------------
+// class ExampleClass{
+//     public function saySomething(){
+//       return 'Hello Bangladesh';
+//     }
+//   }
+
+// $obj = new ExampleClass(); /////non static method take call korte hole tar class take ai vabe object create kore nite hoy
+
+// dd($obj->saySomething());
+
+
+//// OR-------------------------------
+
+// class ExampleClass{
+//     private function saySomething(){
+//         return 'Hello Bangladesh';
+//     }
+
+//     public static function __callStatic(string $name, array $arguments)
+//     {
+//         return call_user_func_array([new static , $name] , $arguments);
+//     }
+// }
+
+// dd(ExampleClass::saySomething());
+
+
+/////// How laravel facade works example------------
+
+class  ExampleService{
+    public function saySomething(): string
+    {
+        return 'Hello Dip';
+    }
+}
+
+class ExampleServiceFacade extends Facade{
+
+    public static function getFacadeAccessor(): string
+    {
+        return "exampleService";
+    }
+    
+}
+
+
+class Facade{
+
+   public static function __callStatic(string $name , array $arguments)
+   {
+      //dd($name);
+      //dd($arguments);
+      return app(static::getFacadeAccessor())->$name(...$arguments);
+   }
+
+   public static function getFacadeAccessor(): string 
+   {
+    return "";
+   }
+}
+
+app()->bind("exampleService",function() {  ////akhane ami bind ar maddhome service container a add korechi
+    return new ExampleService(); //// new ar maddhome ExampleService ar akta instance create kore service container a add kore rakhbe 
 });
+
+
+//dd(app()->make("exampleService")); ///// service container a bind kora ba add kora service gulo dekhar jonno ba retrait korar jonno amra make mathod use kori
+//dd(app('exampleService'));
+/////facade--------------------
+//dd(ExampleServiceFacade::saySomething());
+dd(ExampleServiceFacade::saySomething(125684,568)); ////uncomment //dd($name);
+//dd($arguments); and watch browser
+
+
+
+
+
+
+
+// Route::get('/', function () {
+//     dd(Config::get(key:'app.name'));////non static method take amra akhane static vabe call korchi ////it's comes from vendor laravel/framework/src/illuminate/Config/Repository.php 
+    
+// });

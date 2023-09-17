c4d33f345e53af40b404bee573ed58431dac990d
DipItlogiko <dipitlogiko@gmail.com>
2023-09-17 16:40:14 +0600

    URL_Generation part1

--- Practice_laravel/README.md
+++ Practice_laravel/README.md
@@ -1,1 +1,56 @@
 # Laravel-10Documentation
+
+## URL Generation ([Tutorial](https://www.youtube.com/watch?v=_kkM802vgbI&t=145s))
+
+Laravel provides several helpers to assist you in generating URLs for your application. These helpers are primarily helpful when building links in your templates and API responses, or when generating redirect responses to another part of your application.
+
+## The Basics ([Document](https://laravel.com/docs/10.x/urls#the-basics))
+
+
+## URLs For Named Routes([Document](https://laravel.com/docs/10.x/urls#urls-for-named-routes))
+
+
+## Signed URLs([Document](https://laravel.com/docs/10.x/urls#signed-urls))
+
+
+signed URLs  ar maddhome amara kono   route ke private kore dite pari mane amra oi route ar akta signuture diye dite pari.jodi kew amader oi singeture url a hit kore sora sori tahole kaj korbe na kintu kew jodi oi url ar sathe signeture add kore tahole she oi page ta access korte parbe ....
+
+**Signed URLs ar kichu methods:**
+
+ * URL::signedRoute('name_route_ar_nam')
+
+   URL:: fachads ar signedRoute() ai method ta akta name route ar nam receive kore....and oi name route take mane ami signedRoute(ar moddhe jei name route ar nam dibo oi route ar akta signature key generate kore it looks like signature=44cfbfc9ced09220ccc09090db9d8e3885173370828f9011b48a775d6e42134b) 
+
+
+ * URL::temporarySignedRoute('name_route_ar_nam',time,pass data to the route)
+
+   temporarySignedRoute() akta name route ar nam receive kore tar porer paramiter a time receive kore jemon now()->addMinutes(30) or now()->addSeconds(13) or now()->addHours(5) and tar porer paramiter ba third patamiter a amra amader name route a data pass korte pari jemon ['user' => 1] ......and oi route aa jei page ta thake ami jodi chai je oi page ta kichukon por expair hoye jabe mane ami jei time a set kore debo tar por oi page ta expair hoye jabe tokhon amra ai method ta use korte pari...ai method ta muloto email varification page aaaa use kora hoy
+
+  Check routes/web.php
+
+## Responding To Invalid Signed Routes([Document](https://laravel.com/docs/10.x/urls#responding-to-invalid-signed-routes))
+
+ check routes/web.php
+
+
+
+##  URLs For Controller Actions([Document](https://laravel.com/docs/10.x/urls#urls-for-controller-actions))
+
+Check routes/web.php
+
+
+## Default Values([Document](https://laravel.com/docs/10.x/urls#default-values))
+
+ar maddhome amar url paramiter ar akta default value set kore dite pari 
+
+###  URL Defaults & Middleware Priority([Document](https://laravel.com/docs/10.x/urls#url-defaults-middleware-priority))
+
+   amra url paramiter ar akta default value set kore dewar por ai kaj ta korte hobe 
+
+
+
+ 
+
+
+
+
--- Practice_laravel/routes/web.php
+++ Practice_laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//////////////// Accessing The Current URL---------------------------
+
 Route::get('/', function () {
+    //dd(url()->current()); ////////////// amra aikhane current url check korechi 
+    
+    //dd(url()->full()); ////////aikhane amra full url ta pabo
+
+    //dd(url()->previous()); ////////////////// aikhane amra previous url ta check korchi aage jemon amar ai url ta chilo / 
+     
+    //dd(URL::current()); ////////////////// amra aikhane URL Facades bebohar kore current url ta dekhchi
     return view('welcome');
 });
+
+
+/////////////// Signed URLs-------------------------
+///////(for this practice you should uncoment the / route)
+//////////(go welcome.blade.php)
+
+Route::get('/secret',function(Request $request){
+    if (! $request->hasValidSignature()) { /////jodi amader url a kono valid signutare na thake tahole ooo 401 error throw korbe ami akhane chile kono custom blade file create kore ooo return kore dite pari sorasori eeerror throw na kore
+        abort(401);
+    }
+
+    return "this is a secret message"; ///// r jodi valid singature thake amader url a tahole ooo ai ta return korbe 
+})->name('secret');
+
+
+/////////// OR
+////////(this is the another method of Signed URLs which name is temporarySignedRoute go READEME.md for mor information)
+
+// Route::get('/secretPage',function(Request $request){
+//     if (! $request->hasValidSignature()) {  
+//         return view('expaired'); 
+//     }
+
+//     return "this is a secret message"; ///// r jodi valid singature thake amader url a tahole ooo ai ta return korbe 
+// })->name('secretPage');
+
+/////// OR (we can replace above if statement with middleware....by default laravel provide a middleware which name is signed....the middleware alias is located into the app/Http/kernel.php 
+
+Route::get('/secretPage',function(Request $request){
+    return "this is a secret message"; 
+})->name('secretPage')->middleware('signed'); //////// signed middleware ta check korbe je amader url a kono signed ache ki na jodi amader url theke kono signed key na ashe tahole ooo akta eerror throw korbe jodi amra oi eeerror take customize korte chai tahole check (Responding To Invalid Signed Routes) this section in READEME.md of this  Document
+
+
+/////////////////// URLs For Controller Actions-------------------
+
+Route::get('/URLS',function(Request $request){
+    return view('test');
+}); 
+Route::get('/sayHello', [HomeController::class, 'sayHello'])->name('sayHello');

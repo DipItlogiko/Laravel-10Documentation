a321f91562e2da871802405921bb976ac104d05e
DipItlogiko <dipitlogiko@gmail.com>
2023-09-21 22:45:14 +0600

    .

--- Practice_laravel/README.md
+++ Practice_laravel/README.md
@@ -1,1 +1,37 @@
 # Laravel-10Documentation
+
+## Collections
+
+ The Illuminate\Support\Collection class provides a fluent, convenient wrapper for working with arrays of data. 
+
+ mane hocche
+
+ kono data ar array [] niye kaj korte hole amra oi  khetree collection ar bebohar korbo. 
+
+
+## Creating Collections([Document](https://laravel.com/docs/10.x/collections#creating-collections)) 
+
+collect() helper methods ar moddhe amra jei array [] ta pass korbo oi array ar jonno collect() method Illuminate\Support\Collection class ar akta instance create korbe.
+
+
+check routes/web.php
+
+
+## Extending Collections([Document](https://laravel.com/docs/10.x/collections#extending-collections))
+
+check routes/web.php
+
+
+## Macro Arguments([Document](https://laravel.com/docs/10.x/collections#macro-arguments))
+
+check routes/web.php
+
+## Available Methods([Document](https://laravel.com/docs/10.x/collections#available-methods))
+
+amra jani jokhon amra kono data ar array niye kaj korbo Tokhon amra Collection use korbo.
+
+ai Collection ar kichu methods ache jar maddhome amra amader array ar moddhe kichu operation korte pari khub sohoje:
+
+![image](./Screenshot%20from%202023-09-21%2022-11-11.png)
+
+check the Document
--- Practice_laravel/routes/web.php
+++ Practice_laravel/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Lang;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/////////////////// Creating Collections-----
+/////////(check READEME.md)
+
 Route::get('/', function () {
-    return view('welcome');
+    $collection = collect([1, 2, 3]); 
+
+    dd($collection); ////// amra browser aa dekhte pabo Illuminate\Support\Collection ai class ar akta instance create koreche amader [1,2,3] ai array ta karon amra ai array take collect() helper method ar moddhe likhe diyechi tai.....collect() method ar moddhe amra jei array take pass korbo collect() method Illuminate\Support\Support\Collection ai class ar akta instance create korbe collect() method ar vetore thaka array ar jonno 
 });
+
+
+///////////////// Extending Collections---------
+////////////(check READEME.md for more information)
+
+Route::get('/test', function () {
+
+    Collection::macro('textUpper', function () { //////// akhane ami Collection class ar sathe akta method use korechi jar nam macro ai macro method ar kaj hocche oooo akta clouser accept kore and ai clouser ta execute hobe jokhon kew macro ke call korbe jemon ami amar ai mecro method ar akta name diye diyechi textUpper. aikhane je kono name dewa jai.jokon ami amar ai macro method take call korbo textUpper ai name tokhon ai macro mathod ar closure ta execute hobe
+        return $this->map(function (string $value) {
+            return Str::upper($value);
+        });
+    });
+
+    $collection = collect(['first', 'second']);
+ 
+    $upper = $collection->textUpper(); ////// akhane ami $collection->textUpper(); $collection ar moddhe je data gulo ache ta ami amar macro method ar closer ar moddhe pathiye dicchi $collection variable ar sathe macro method take call kore ->textUpper
+
+    dd($upper);
+});
+
+
+//////////////////// Macro Arguments-----------
+
+Route::get('/dip',function(){
+
+    Collection::macro('toLocale', function (string $locale) {
+        return $this->map(function (string $value) use ($locale) {
+            return Lang::get($value, [], $locale);
+        });
+    });
+     
+    $collection = collect(['first', 'second']);
+     
+    $translated = $collection->toLocale('es');
+
+    dd($translated);
+});
+
+
+

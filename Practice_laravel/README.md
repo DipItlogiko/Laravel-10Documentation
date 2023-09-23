a7d8ffd0aa1602017723aa35a79042c2f3b282a2
DipItlogiko <dipitlogiko@gmail.com>
2023-09-23 16:53:29 +0600

    Done

--- Practice_laravel/README.md
+++ Practice_laravel/README.md
@@ -11,6 +11,8 @@
 
 ## Creating Collections([Document](https://laravel.com/docs/10.x/collections#creating-collections)) 
 
+amra kono collection create kori Illuminate\Support\Collection ai class ar akta helper method ar maddhome jar nam collect()
+
 collect() helper methods ar moddhe amra jei array [] ta pass korbo oi array ar jonno collect() method Illuminate\Support\Collection class ar akta instance create korbe.
 
 
@@ -34,4 +36,99 @@ ai Collection ar kichu methods ache jar maddhome amra amader array ar moddhe kic
 
 ![image](./Screenshot%20from%202023-09-21%2022-11-11.png)
 
-check the Document
+Mast check the Document
+
+## Method Listing([Document](https://laravel.com/docs/10.x/collections#method-listing))
+
+ * all() 
+
+  ai method ar kaj hocche amader array ar sob data dekhabe
+
+  check routes/web.php
+
+
+ * avg()
+
+   ai method ar kaj hocche amader array ar value ar akta avarage number ber kora.
+
+
+ * chunk()
+
+  ai method ar kaj hocche amra chunk(ar moddhe je number ta pass korbo oi number onujayi amader array ar value gulo vag kore debe)  
+
+
+ * dd()
+
+ ai ta hochhe die and dump aita amra amader application ar jeikhane use korbo tar por theke amader application ar code gulo r execute hobe na.
+
+ jodi amra na chai amader application ar code gulo execute na hok tahole amra shudhu dump() method use korbo ....amra amader application ar jeikhane dump() method ta use korbo tar por thekeoooo amader application ar code execute hobe .
+
+
+ * first()
+
+  ai method ar kaj hocche jodi ooo prothome kono data pai tahole dekhabe
+
+
+ * firstOrFail() 
+
+  ai method ar kaj hocche jodi ooo ptothome kono data pai tahole dekhabe and jodi  na pai tahole akta error throw korbe Illuminate\Support\ItemNotFoundException.
+
+
+ * pipeThrough()
+
+   ai method ar  moddhome amra multiple function run korate pari ar moddhe. (check document)
+
+ * random()
+
+  ai method ta bebohar kore amra amader array theke ak ak somoy ak ak ta value print korate pari ...and jodi amra random(ar moddhe kono number pass kore dei jomon 2 ba 3 ja khushi tahole oooo amader array theke protibar 2 ta ba 3 ta kore value niye amader dekhabe) ....kintu jodi amader array te 2 ba 3 ta value ta thake r ami jodi random(ar modddhe 2 ba 3 pass kori tahole ) ai error ta InvalidArgumentException throw korbe 
+
+
+ * pop()
+
+   ar kaj hocche amader array ar last ar dik theke 1 ta kore value remove kore debe.....jodi ami pop(ar moddhe kono number pass kori jemon 1 ba 3 jai ee hok na keno oooo amar array ar last ar dik theke 1 kore ba 3 ta kore value remove kore debe)
+
+
+ * shift()
+
+  ar kaj hocche amader array ar prothom ar dik theke 1 ta kore data remove  kore debe.....jodi ami shift(ar moddhe kono number pass kori jemon 1 ba 3 jai ee hok na keno ooo amar array ar prothom dik theke 1 kore ba 3 ta kore vlue remove kore debe )  
+
+
+ * skip()
+
+   ar kaj hocche amader array ar prothom theke value guloke remove kore debe...amra skip(ar moddhe jei number pass korbo oooo amader array ar prothom theke oi koita value skip kore bad baki gulo dekhabe)
+
+
+## Higher Order Messages([link](https://laravel.com/docs/10.x/collections#higher-order-messages))(IMPORTANT)      
+
+
+## Lazy Collections([link](https://laravel.com/docs/10.x/collections#lazy-collections))([tutorial](https://www.youtube.com/watch?v=jCiypgzg9os&t=197s))
+
+amra kom memory use kore data ar boro set niye kaj korte parbo ai Lazy Collections ar maddhome... mane oonek data gulo amara less memory consumption diye kaj korte parbo ai lazy collections ar maddhome.
+
+
+* for example:
+
+ jemon dhoren amader User model ar users table aaa  oonek user ar data thakte pare 1 lakh 2 lakh ba tar theke ooo beshi...to jokhon amra User::all() ai ta call korbo tokhon amar database ar users table theke sob user ar data amader laravel ar memory storage a load kore nei beshi data laravel ar memory storage load hole laravel ar memory storage limit cross hoye gele laravel akta error throw korbe...ai problem ta solve korar jonno amra Lazy Collection ta use korbo...
+
+* cursor()
+
+  ai method ta Lazy Collection ar akta instance create kore
+
+amra User::cursor() aivabe likhle Lazy Collection ar akta instance create korbe User moddle ar jonno
+
+
+Check routes/web.php
+
+* ta chara jodi amader eemon hoy je amader application a akta 10 lokkho line ar bar tar bishi line ar  akta text file ache...and shei text file take jodi bydefault vabe read kori tahole amader laravel ar memory storage aaa sob line gulo load hobe and beshi line amader laravel ar memory storage aaa load howar karone amader laravel ar memory storage limitation cross hote pare jar fole laravel amader akta error throw korbe and application crash korbe.     
+
+ chack routes/web.php (text file handel with lazy collection) section
+
+
+
+## Lazy Collection Methods([link](https://laravel.com/docs/10.x/collections#lazy-collection-methods))
+
+
+## the belows topic i don't understand: 
+
+* reduceSpread()([link](https://laravel.com/docs/10.x/collections#method-reduce-spread))
+
--- Practice_laravel/routes/web.php
+++ Practice_laravel/routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Lang;
+use App\Models\User;
 
 
 /*
@@ -21,7 +23,7 @@ use Illuminate\Support\Facades\Lang;
 /////////(check READEME.md)
 
 Route::get('/', function () {
-    $collection = collect([1, 2, 3]); 
+    $collection = collect([1, 2, 3]);  ///// amra aivabe collection create kori
 
     dd($collection); ////// amra browser aa dekhte pabo Illuminate\Support\Collection ai class ar akta instance create koreche amader [1,2,3] ai array ta karon amra ai array take collect() helper method ar moddhe likhe diyechi tai.....collect() method ar moddhe amra jei array take pass korbo collect() method Illuminate\Support\Support\Collection ai class ar akta instance create korbe collect() method ar vetore thaka array ar jonno 
 });
@@ -62,6 +64,138 @@ Route::get('/dip',function(){
 
     dd($translated);
 });
+
+
+
+///////////// Method Listing----------------------------
+////////// all()
+Route::get('/all',function(){
+    $data = collect(['name' => 'dip', 'age' => '20', 'address' => 'Faridpur']);
+
+    $all = $data->all();
+
+    dd($all);
+});
+
+
+///////// avg()
+
+Route::get('/avg',function(){
+
+    // $average = collect([
+    //     ['foo' => 10],
+    //     ['foo' => 10],
+    //     ['foo' => 20],
+    //     ['foo' => 40]
+    // ])->avg('foo');
+
+    // dd($average);
+
+
+
+   //////OR---------------------
+
+//    $average = collect([1, 1, 2, 4])->avg();
+//    dd($average);
+
+
+   $average2 = collect([1, 1, 2, 4, 8, 7]);
+   $result = $average2->avg();
+   dd($result);
+
+
+
+   
+
+
+});
+
+
+
+
+//////////////////////// Lazy Collections----------------
+
+// Route::get('/lazy',function(){
+//     $collection = Collection::times(40); ////// akhane amra Collection facadas ta use korchi ai ai Collection ar sathe akta method use korchi jar nam times ar kaj hocche ami () ar moddhe jei number ta diye debo oooo 1 theke oi number porjonto print korbe....jemon ai khane 1 theke 40 porjonto print korbe
+    
+//     foreach($collection as $col){
+//         echo $col .'<br>';
+//     }
+ 
+// });
+
+
+//////////// kintu ami jodi times(ar moddhe oonek boro number diye dei tahole ooo akta error dekhabe amader browser aa karon laravel ar je memori limitation ache amader collection gulo ke ram aa rakhar jonno and oigulo execution howar jonno shei memori limitation ta sesh hoye jabe ba overflow hoye jabe jodi amra aikhane beshi number dei tahole) 
+///////(ai problem take amra Lazy Collection ar  maddhome solve korte pari)
+
+// Route::get('/lazy',function(){
+// $collection = Collection::times(922337203); ////////akhane times() amader oonek gulo callection create korbe ja amader laravel ar collection ar memory limite ar oopore chole jabe tai ai ta amader akta error throw korbe  
+    
+//     foreach($collection as $col){
+//         echo $col .'<br>';
+//     }
+ 
+// });
+
+
+/////////// solve this above problem with Lazy Collection
+ 
+Route::get('/lazy',function(){
+    $collection = LazyCollection::times(922337203);  
+        
+        foreach($collection as $col){
+            echo $col .'<br>';
+        }
+
+        return 'Done';
+     
+    });
+
+///////////// cursor()
+/////(CHECK READEME.md)
+
+Route::get('/dip',function(){
+    $allUser = User::all();/////// akhane amader database ar users table aaa joto user ar data ache sob ak sathe amader laravel ar memory storage a load hobe. jokhon amader ai table aa lakh lakh user ar data thakbe tokhon  amra aivabe korle amader laravel ar memory storage limitation cross hoye jabe and amader akta error dekhabe tai amra aita ke aivabe na kore Lazy Collection ar cursor method ar  maddhome korbo
+    dd($allUser);
+}); 
+
+
+// Route::get('/cursor',function(){
+//     $allUser = User::cursor();/////// ai cursor() method ta Lazy Collection ar akta instance create korbe User model ar jonno....akhon jodi amar users table a 10 lokkho ba 1 billion  oooo data thake tahole ooo amader laravel ar memory storage limitation cross hobe na. 
+//     dd($allUser->first()); ///// aikhane ami amar $allUser variable ar moddhe je data gulo ache or moddhe first a je data ta pabe ta show korbe
+// }); 
+
+/////OR-------
+Route::get('/cursor',function(){
+    $allUser = User::cursor();/////// ai cursor() method ta Lazy Collection ar akta instance create korbe User model ar jonno....akhon jodi amar users table a 10 lokkho ba 1 billion  oooo data thake tahole ooo amader laravel ar memory storage limitation cross hobe na. 
+    
+    foreach($allUser as $user) /////// jehetu $allUser variable ar moddhe akta array pabo rrrr amra jani amra jokhon kono array ar value ke print korbo tokhon amra foreach loop use korbo
+        echo $user->name .'<br/>';
+}); 
+
+
+
+/////////////////// text file handel with lazy collection---------------------------
+
+Route::get('/textFile',function(){
+
+    $collection = LazyCollection::make(function(){ /////// akhane akta Lazy Collection make kora hoyeche LazyCollection fasad ar sathe make method ta ke call kore.
+        $handle = fopen(storage_path() . '/logs/laravel.log', 'r'); /////fopen() ai ta hocche php ar akta function jar maddhome amra kono file ke open kori r tar por 'r' mane ami bole dilam ami ai file take read korte chacchi
+
+        while (($line = fgets($handle)) !== false) {
+            yield $line;
+
+        }
+
+    });
+
+    foreach ($collection as $line) //////jehetu $collection variable ar moddhe akta array ke pabo r amra jani kono array ar value print korte amara foreach ba forelse loop use kori
+      echo $line."<br/>";
+
+
+   
+});
+
 
 
 
